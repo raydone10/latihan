c22ea43e861d5d988d19fd60b48dafa6c5e044ad
AryaGithub123 <aryamaulana32743@gmail.com>
2024-08-13 14:48:39 +0700

    edit placeholder

--- resources/views/formhitung.blade.php
+++ resources/views/formhitung.blade.php
@@ -2,7 +2,7 @@
 <body>
 <form method="POST" action="{{url('formhitung');}}">
       @csrf
-    <input type="text" name ="panjang"placeholder="Isi Panjang"><br><br>
+    <input type="text" name ="panjang"placeholder="Longgg"><br><br>
     <input type="text" name ="lebar"placeholder="Isi Panjang"><br><br>
     <input type="submit" value="proses">
 </form>
